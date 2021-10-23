15ea27602cd4262a2ac129ca353851724c85dcb7
Anna Krivopusk <anna.krivopusk@t-systems.com>
2021-10-23 23:53:16 +0300

    implement JdbcMealRepository with Spring JDBC Template

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -28,11 +28,11 @@ CREATE TABLE user_roles
 CREATE TABLE meals
 (
     id               INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
-    datetime         TIMESTAMP                         NOT NULL,
+    date_time         TIMESTAMP                         NOT NULL,
     description      VARCHAR                           NOT NULL,
     calories         INTEGER                           NOT NULL,
     user_id          INTEGER                           NOT NULL,
-    CONSTRAINT meal_datetime_idx UNIQUE (user_id, datetime),
+    CONSTRAINT meal_datetime_idx UNIQUE (user_id, date_time),
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
 CREATE INDEX user_meals_idx ON meals (user_id);
