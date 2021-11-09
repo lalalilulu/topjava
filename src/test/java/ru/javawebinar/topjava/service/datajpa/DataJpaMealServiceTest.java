857f260ae11617374b79b234c1c4818d4bf142a7
Anna Krivopusk <anna.krivopusk@t-systems.com>
2021-11-09 17:36:47 +0300

    HW5: create common abstract test classes and tests for all spring profiles

---
+++ src/test/java/ru/javawebinar/topjava/service/datajpa/DataJpaMealServiceTest.java
@@ -0,0 +1,9 @@
+package ru.javawebinar.topjava.service.datajpa;
+
+import org.springframework.test.context.ActiveProfiles;
+import ru.javawebinar.topjava.service.MealServiceTest;
+
+@ActiveProfiles("datajpa")
+public class DataJpaMealServiceTest extends MealServiceTest {
+
+}
---
+++ src/test/java/ru/javawebinar/topjava/service/datajpa/DataJpaUserServiceTest.java
@@ -0,0 +1,9 @@
+package ru.javawebinar.topjava.service.datajpa;
+
+import org.springframework.test.context.ActiveProfiles;
+import ru.javawebinar.topjava.service.UserServiceTest;
+
+@ActiveProfiles("datajpa")
+public class DataJpaUserServiceTest extends UserServiceTest {
+
+}
