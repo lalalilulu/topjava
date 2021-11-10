900efa5a06b70e020df4502084fea2aed46ab452
Anna Krivopusk <anna.krivopusk@t-systems.com>
2021-11-10 11:09:24 +0300

    HW5: use FetchType.LAZY for meals and solve Hibernate Lazy-Init issue

--- src/main/java/ru/javawebinar/topjava/model/User.java
+++ src/main/java/ru/javawebinar/topjava/model/User.java
@@ -55,7 +55,7 @@ public class User extends AbstractNamedEntity {
     private int caloriesPerDay = DEFAULT_CALORIES_PER_DAY;
 
     //https://www.baeldung.com/jpa-joincolumn-vs-mappedby
-    @OneToMany(mappedBy = "user", fetch = FetchType.EAGER)
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
     //https://www.baeldung.com/jpa-sort
     @OrderBy("dateTime DESC")
     private List<Meal> meals;
--- src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/CrudUserRepository.java
@@ -17,6 +17,7 @@ public interface CrudUserRepository extends JpaRepository<User, Integer> {
 
     User getByEmail(String email);
 
-    @Query("SELECT u FROM User u WHERE u.id=:id")
+    //https://thorben-janssen.com/lazyinitializationexception/#Don8217t_use_FetchTypeEAGER
+    @Query("SELECT u FROM User u LEFT JOIN FETCH u.meals WHERE u.id=:id")
     User getWithMeals(@Param("id") int id);
 }
